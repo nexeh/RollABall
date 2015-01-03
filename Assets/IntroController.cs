6f6b18bbdb6e19a6c3bf5b3b21da9a56cf4ddf2f
Jeremy Corson <jeremy.r.corson@gmail.com>
2015-01-02 21:38:39 -0500

    added a splash screen

---
+++ Assets/IntroController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroController : MonoBehaviour {
+
+	public string level;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine( Order() );
+	}
+
+	IEnumerator Order() {
+		yield return new WaitForSeconds (3.0f); 
+		Application.LoadLevel(level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
